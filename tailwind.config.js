cd0e8a5ae043cf93839b5ddfb0b95413b34fd389
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2024-08-01 15:47:49 -0300

    styles(add base styles)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '@tablet': '40rem',
+        '@laptop': '64rem',
+        '@desktop': '80rem',
+      },
+      fontFamily: {
+        'title': ['var(--font-title)', 'sans-serif'],
+        'body': ['var(--font-body)', 'sans-serif'],
+      },
+      colors: {
+        "brand-dark-gray-100": "#1f1f1f",
+        "brand-dark-gray-200": "#474747",
+        "brand-dark-gray-300": "#666666",
+
+        "brand-light-gray-100": "#8f8f8f",
+        "brand-light-gray-200": "#cccccc",
+        "brand-light-gray-300": "#f5f5f5",
+
+        "brand-primary": "#c92071",
+        "brand-secondary": "#b5b6f2",
+        "brand-tertiary": "#991956",
+
+        "brand-error": "#ee4266",
+        "brand-success": "#52ca76",
+        "brand-warning": "#f6aa1c",
+        "brand-white": "#ffffff",
+      },
+    },
   },
   plugins: [],
 }
